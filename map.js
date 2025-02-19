d79ff12d9b4a78823e095e643c59bc62c570833e
Kevin Souder <129797279+kesouder@users.noreply.github.com>
2025-02-18 22:32:03 -0800

    updated map.js

--- map.js
+++ map.js
@@ -4,8 +4,8 @@
    // Initialize the map
     const map = new mapboxgl.Map({
         container: 'map', // ID of the div where the map will render
-        // style: 'mapbox://styles/mapbox/streets-v12', // Map style
-        style: 'mapbox://styles/mapbox/navigation-day-v1', // alternative map style
+        style: 'mapbox://styles/mapbox/streets-v12', // Map style
+        // style: 'mapbox://styles/mapbox/navigation-day-v1', // alternative map style
         center: [-71.09415, 42.36027], // [longitude, latitude]
         zoom: 12, // Initial zoom level
         minZoom: 5, // Minimum allowed zoom
